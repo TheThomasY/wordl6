55e036d487a040b619aa4274d4f941edeb0a0d38
TheThomasY <twy1g16@soton.ac.uk>
2022-02-07 16:26:21 +0000

    feat: header added with title

---
+++ components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './Header.module.scss';
+
+export default function Header() {
+  return (
+    <div className={styles.header}>
+      <h1 className={styles.title}>Wordl6</h1>
+    </div>
+  );
+}
