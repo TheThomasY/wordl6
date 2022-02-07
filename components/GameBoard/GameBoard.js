29e2183e21c321c89ce63ba485d797e1df7f38d9
TheThomasY <twy1g16@soton.ac.uk>
2022-02-07 18:18:25 +0000

    feat: rows as component, generates from array

--- components/GameBoard/GameBoard.js
+++ components/GameBoard/GameBoard.js
@@ -1,59 +1,18 @@
 import React from 'react';
 
+import Row from './Row';
+
 import styles from './GameBoard.module.scss';
 
 export default function GameBoard() {
+  const rows = [1, 2, 3, 4, 5, 6];
+
   return (
     <div className={styles.gameBoard}>
       <div className={styles.gameGrid}>
-        <div className={styles.row}>
-          <div className={styles.tile}></div>
-          <div className={styles.tile}></div>
-          <div className={styles.tile}></div>
-          <div className={styles.tile}></div>
-          <div className={styles.tile}></div>
-          <div className={styles.tile}></div>
-        </div>
-        <div className={styles.row}>
-          <div className={styles.tile}></div>
-          <div className={styles.tile}></div>
-          <div className={styles.tile}></div>
-          <div className={styles.tile}></div>
-          <div className={styles.tile}></div>
-          <div className={styles.tile}></div>
-        </div>
-        <div className={styles.row}>
-          <div className={styles.tile}></div>
-          <div className={styles.tile}></div>
-          <div className={styles.tile}></div>
-          <div className={styles.tile}></div>
-          <div className={styles.tile}></div>
-          <div className={styles.tile}></div>
-        </div>
-        <div className={styles.row}>
-          <div className={styles.tile}></div>
-          <div className={styles.tile}></div>
-          <div className={styles.tile}></div>
-          <div className={styles.tile}></div>
-          <div className={styles.tile}></div>
-          <div className={styles.tile}></div>
-        </div>
-        <div className={styles.row}>
-          <div className={styles.tile}></div>
-          <div className={styles.tile}></div>
-          <div className={styles.tile}></div>
-          <div className={styles.tile}></div>
-          <div className={styles.tile}></div>
-          <div className={styles.tile}></div>
-        </div>
-        <div className={styles.row}>
-          <div className={styles.tile}></div>
-          <div className={styles.tile}></div>
-          <div className={styles.tile}></div>
-          <div className={styles.tile}></div>
-          <div className={styles.tile}></div>
-          <div className={styles.tile}></div>
-        </div>
+        {rows.map((row) => (
+          <Row key={row} />
+        ))}
       </div>
     </div>
   );
