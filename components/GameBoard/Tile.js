73d908724f932efd98ba34400a792aadac258985
TheThomasY <twy1g16@soton.ac.uk>
2022-02-10 16:28:47 +0000

    feat: added spin animation to match reveal on board

--- components/GameBoard/Tile.js
+++ components/GameBoard/Tile.js
@@ -6,12 +6,14 @@ import styles from './Tile.module.scss';
 export default function Tile(props) {
   let tileStyle = {};
 
-  if (props.matches[props.letter] === 0) {
-    tileStyle = { backgroundColor: '#3e9f1c', color: 'white' };
-  }
+  console.log(Object.keys(props.matches).length);
 
-  if (props.matches[props.letter] === 1) {
-    tileStyle = { backgroundColor: '#c39318', color: 'white' };
+  if (props.matches[props.letter] === 0) {
+    tileStyle = { backgroundColor: '#3e9f1c', color: 'white', border: 'none' };
+  } else if (props.matches[props.letter] === 1) {
+    tileStyle = { backgroundColor: '#c39318', color: 'white', border: 'none' };
+  } else if (props.matches[props.letter] === -1) {
+    tileStyle = { backgroundColor: '#787c7e', color: 'white', border: 'none' };
   }
 
   return (
@@ -21,7 +23,11 @@ export default function Tile(props) {
         ' ' +
         (props.letter !== ' '
           ? styles['tile-active'] + ' ' + styles['scale-up-center']
-          : '')
+          : '') +
+        ' ' +
+        (Object.keys(props.matches).length === 0
+          ? ''
+          : styles['flip-horizontal-top'])
       }
       style={tileStyle}
     >
