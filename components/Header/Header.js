51864be7c0f160df89f80f8f80faed57377b44b8
TheThomasY <twy1g16@soton.ac.uk>
2022-02-17 15:07:53 +0000

    feat: toggle modal

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
+// Css
 import styles from './Header.module.scss';
 
-export default function Header() {
+// React Icons
+import { IoIosStats } from 'react-icons/io';
+
+export default function Header(props) {
+  const toggleStats = () => {
+    props.toggleStats();
+  };
+
   return (
     <div className={styles.header}>
       <h1 className={styles.title}>Wordl6</h1>
+      <div onClick={toggleStats}>
+        <IoIosStats size={'2rem'} />
+      </div>
     </div>
   );
 }
--- components/UI/Modal.js
+++ components/UI/Modal.js
@@ -3,11 +3,20 @@ import React, { useState, useEffect } from 'react';
 // Css
 import styles from './Modal.module.scss';
 
-export default function PopUp() {
+// React Icons
+import { AiOutlineClose } from 'react-icons/ai';
+
+export default function PopUp(props) {
+  const toggleStats = () => {
+    props.toggleStats();
+  };
+
   return (
     <div className={styles.overlay}>
       <div className={styles.modal}>
-        <div className={styles.close}>x</div>
+        <div onClick={toggleStats} className={styles.close}>
+          <AiOutlineClose size={'2rem'} />
+        </div>
         <h2>Statistics</h2>
       </div>
     </div>
